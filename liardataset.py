587f9a0dca2a814ffcab682712c976a649bf6513
Douglas <douglascastrorj@gmail.com>
2023-03-26 11:43:14 -0300

    load liar dataset

--- liardataset.py
+++ liardataset.py
@@ -1,3 +1,13 @@
 from datasets import load_dataset
+from Dataset import Dataset
+from transformers import BertTokenizer, GPT2Tokenizer
 
-dataset = load_dataset("liar")
+#type = train / test / validation
+def loadLiarDataset(tokenizer, type):
+
+    dataset = load_dataset("liar")[type]
+    # print(dataset)
+
+    df = {'label': dataset['label'], 'text': dataset['statement']}  
+   
+    return df
