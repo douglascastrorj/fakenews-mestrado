92e2616ab08188ca9d3a8b16159f961dfe79062d
Douglas Castro <douglascastrorj@gmail.com>
2023-03-30 08:11:17 -0300

    liar binario acuracia bert 0.641 gpt2 0.602

--- experimentos.py
+++ experimentos.py
@@ -13,21 +13,8 @@ from transformers import BertTokenizer, GPT2Tokenizer
 import time
 from liardataset import loadLiarDataFrame, getLiarLabels
 
-datapath = 'liar-test.csv'
-df = pd.read_csv(datapath)
-
-
-# BERT_MODEL = 'neuralmind/bert-base-portuguese-cased'  #'n
-GPT_MODEL = 'gpt2'# 'pierreguillou/gpt2-small-portuguese' #
-
-def getGPTTokenizer():
-    tokenizer = GPT2Tokenizer.from_pretrained(GPT_MODEL)
-    tokenizer.padding_side = "left"
-    tokenizer.pad_token = tokenizer.eos_token
-    return tokenizer
-
-# tokenizer = BertTokenizer.from_pretrained(BERT_MODEL) 
-tokenizer = getGPTTokenizer()
+# datapath = 'liar-test.csv'
+# df = pd.read_csv(datapath)
 
 
 # labels = {'fake':0,
@@ -37,15 +24,30 @@ tokenizer = getGPTTokenizer()
 labels = getLiarLabels(binary=True)
 
 NUM_EXPERIMENTS = 1
-EPOCHS = 5
-LR = 1e-6
+EPOCHS = 10
+LR = 1e-4
+
+#TREINANDO GPT
+
+print('TREINANDO GPT2')
+GPT_MODEL = 'gpt2'# 'pierreguillou/gpt2-small-portuguese' #
+
+
+def getGPTTokenizer():
+    tokenizer = GPT2Tokenizer.from_pretrained(GPT_MODEL)
+    tokenizer.padding_side = "left"
+    tokenizer.pad_token = tokenizer.eos_token
+    return tokenizer
+
+tokenizer = getGPTTokenizer()
+
 for i in range(0, NUM_EXPERIMENTS):
     print('Running experiment: #' + str( i + 1))
 
-    np.random.seed(int(time.time()))
-    df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=int(time.time())), 
-                                        [int(.8*len(df)), int(.9*len(df))])
-    # df_train, df_val, df_test = loadLiarDataFrame()
+    # np.random.seed(int(time.time()))
+    # df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=int(time.time())), 
+    #                                     [int(.8*len(df)), int(.9*len(df))])
+    df_train, df_val, df_test = loadLiarDataFrame()
 
     # model = BertClassifier(BERT_MODEL)
     model = SimpleGPT2SequenceClassifier(num_classes=2, gpt_model_name=GPT_MODEL)
@@ -53,6 +55,29 @@ for i in range(0, NUM_EXPERIMENTS):
     trainer.train(model, df_train, df_val, LR, EPOCHS, batch_size=2)
 
     torch.save(model.state_dict(), 'models/'+GPT_MODEL+'-'+datetime.now().strftime("%Y-%m-%dT%H:%M")+'.model')
+
+    evaluator = ModelEvaluator(tokenizer, labels)
+    evaluator.evaluate('GPT', model, df_test)
+
+
+print('TREINANDO BERT')
+BERT_MODEL =  'bert-base-multilingual-cased' # 'neuralmind/bert-base-portuguese-cased'  #'n
+
+tokenizer = BertTokenizer.from_pretrained(BERT_MODEL) 
+# TREINANDO BERT
+for i in range(0, NUM_EXPERIMENTS):
+    print('Running experiment: #' + str( i + 1))
+
+    # np.random.seed(int(time.time()))
+    # df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=int(time.time())), 
+    #                                     [int(.8*len(df)), int(.9*len(df))])
+    df_train, df_val, df_test = loadLiarDataFrame()
+
+    model = BertClassifier(BERT_MODEL)
+    trainer = ModelTrainer(tokenizer, labels)  
+    trainer.train(model, df_train, df_val, LR, EPOCHS, batch_size=2)
+
+    torch.save(model.state_dict(), 'models/'+BERT_MODEL+'-'+datetime.now().strftime("%Y-%m-%dT%H:%M")+'.model')
 
     evaluator = ModelEvaluator(tokenizer, labels)
     evaluator.evaluate('GPT', model, df_test)
